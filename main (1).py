afc5c3786087b7f7d2d259d4210a7de30197a44f
RAMYAGANESAN-G <144769234+RAMYAGANESAN-G@users.noreply.github.com>
2023-10-04 12:45:27 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,36 +1,24 @@
-class BankAccount:
-    def __init__(self, account_number, account_holder_name, initial_balance=0):
-        self.__account_number = account_number
-        self.__account_holder_name = account_holder_name
-        self.__account_balance = initial_balance
+"""
+Write a function called linear_search_product that takes the list of products and a target product
+name as input. The function should perfrom a linear search to find the target product in the list and
+return a list of indices of all occourrences of the  product if ound,or an empty list if the product is not
+found
+"""
 
-    def deposit(self, amount):
-        if amount > 0:
-            self.__account_balance += amount
-            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
-        else:
-            print("Invalid deposit amount. Please enter a positive amount.")
 
-    def withdraw(self, amount):
-        if amount > 0 and amount <= self.__account_balance:
-            self.__account_balance -= amount
-            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
-        else:
-            print("Invalid withdrawal amount or insufficient balance.")
+def linearSearchProduct(productList,targetproduct):
+  indices = []
 
-    def display_balance(self):
-        print(f"Account Holder: {self.__account_holder_name}")
-        print(f"Account Number: {self.__account_number}")
-        print(f"Account Balance: ${self.__account_balance}")
+  for index,product in enumerate(productList):
+   if product == targetproduct:
+     indices.append(index)
+
+  return indices
 
 
 # Example usage:
-if __name__ == "__main__":
-    # Create a BankAccount instance
-    account = BankAccount("123456789", "John Doe", 1000)
-
-    # Test deposit and withdrawal
-    account.display_balance()
-    account.deposit(500)
-    account.withdraw(200)
-    account.display_balance()
+Products = ["shoes","boot","loafer","shoes","sandal","shoes"]
+target = "shoes"
+target2 = "apple"
+result = linearSearchProduct(Products,target)
+print(result)
